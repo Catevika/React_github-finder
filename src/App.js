d077fb390adfe83c5a12ad1a486029603f1b9d47
Catevika <dominique.bello@outlook.fr>
2022-01-06 09:14:34 +0100

    Add react-router-dom+react-icons & Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 function App() {
 	return (
-		<div>
-			<h1 className='text-xl'>Hello World!</h1>
-		</div>
+		<Router>
+			<div className='flex flex-col justify-between height-screen'>
+				<Navbar />
+			</div>
+		</Router>
 	);
 }
 
