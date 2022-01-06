20ba9ebf7cf8f6c00aa11f2675ebf8ce713a4dd0
Catevika <dominique.bello@outlook.fr>
2022-01-06 12:02:26 +0100

    Add Home, Footer, About, NotFound + Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
 	return (
 		<Router>
 			<div className='flex flex-col justify-between h-screen'>
 				<Navbar />
-				<main className='container mx-auto px-3 pb-12 '>Content</main>
+				<main className='container mx-auto px-3 pb-12 '>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/notfound' element={<NotFound />} />
+						<Route path='/*' element={<NotFound />} />
+					</Routes>
+				</main>
 				<Footer />
 			</div>
 		</Router>
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 function Footer() {
-	const footerYear = new Date().getFullYear;
+	const footerYear = new Date().getFullYear();
 
 	return (
 		<footer className='footer p-10 bg-grey-700 text-primary-content footer-center'>
