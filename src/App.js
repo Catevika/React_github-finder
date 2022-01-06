15ca2355cc3e19f4ba5ac2429646810867aa1ef0
Catevika <dominique.bello@outlook.fr>
2022-01-06 09:28:45 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 
 function App() {
 	return (
 		<Router>
-			<div className='flex flex-col justify-between height-screen'>
+			<div className='flex flex-col justify-between h-screen'>
 				<Navbar />
+				<main className='container mx-auto px-3 pb-12 '>Content</main>
+				<Footer />
 			</div>
 		</Router>
 	);
